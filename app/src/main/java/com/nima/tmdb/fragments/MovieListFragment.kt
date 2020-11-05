ffc900115a5e8b944e31f92c841603490e608847
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-11-05 12:48:43 +0330

    show to all results of searching movie with DiffUtils

--- app/src/main/java/com/nima/tmdb/fragments/MovieListFragment.kt
+++ app/src/main/java/com/nima/tmdb/fragments/MovieListFragment.kt
@@ -14,51 +14,46 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nima.tmdb.R
-import com.nima.tmdb.adapters.MovieRecyclerAdapter
-import com.nima.tmdb.adapters.OnMovieListener
+import com.nima.tmdb.models.Result
+import com.nima.tmdb.ui.MovieListAdapter
 import com.nima.tmdb.utils.Constants
 import com.nima.tmdb.viewModels.MovieListViewModel
+import kotlinx.android.synthetic.main.fragment_movie_list.*
 import java.util.*
 
-class MovieListFragment : Fragment(), OnMovieListener {
+class MovieListFragment : Fragment(), MovieListAdapter.Interaction {
+    private lateinit var movieListAdapter: MovieListAdapter
     private var recyclerView: RecyclerView? = null
     lateinit var mviewModel: MovieListViewModel
-    private var adapter: MovieRecyclerAdapter? = null
     var navController: NavController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mviewModel = ViewModelProvider(this).get(MovieListViewModel::class.java)
         subscribeObservers()
-        initSearchView()
     }
 
     private fun subscribeObservers() {
-        mviewModel.searchMovieAPI.observe(this, Observer { example ->
-            example?.let {
-                mviewModel!!.isMovieRetrieved = true
-                adapter!!.setResults(example.results)
+        mviewModel.searchMovieAPI.observe(this, { example ->
+            example?.let { example ->
+                mviewModel.isMovieRetrieved = true
+                example.results?.let { results ->
+                    movieListAdapter.submitList(results)
+                }
             }
 
         })
     }
 
     private fun initRecyclerView() {
-        adapter = MovieRecyclerAdapter(this)
-        recyclerView!!.adapter = adapter
-        recyclerView!!.layoutManager = LinearLayoutManager(context)
-        recyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                if (!recyclerView.canScrollVertically(1)) {
-                    mviewModel!!.searchNextPage()
-                }
-            }
-        })
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(activity)
+            movieListAdapter = MovieListAdapter(this@MovieListFragment)
+            adapter = movieListAdapter
+        }
     }
 
-    private fun initSearchView() {}
-
     private fun searchMovieAPI(query: String, page: Int, onResume: Boolean) {
-        if (onResume) loadFirstPage() else mviewModel.setMovie(query,page)
+        if (onResume) loadFirstPage() else mviewModel.setMovie(query, page)
     }
 
     private fun loadFirstPage() {
@@ -66,27 +61,28 @@ class MovieListFragment : Fragment(), OnMovieListener {
         val number = random.nextInt(9)
         Log.d(TAG, "RandomNumber: $number")
         try {
-            mviewModel!!.setMovie(Constants.DEFAULT_MOVIE_LIST_NAME[number], Constants.DEFAULT_PAGE)
+            mviewModel.setMovie(Constants.DEFAULT_MOVIE_LIST_NAME[number], Constants.DEFAULT_PAGE)
         } catch (e: ArrayIndexOutOfBoundsException) {
             searchMovieAPI("error", Constants.DEFAULT_PAGE, false)
             Log.e(TAG, "SearchMovieAPI: searchOnResume", e)
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_movie_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-        recyclerView = view.findViewById(R.id.movie_list)
+        recyclerView = view.findViewById(R.id.recycler_view)
         initRecyclerView()
         val searchView: SearchView = view.findViewById(R.id.search_view)
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
-                adapter!!.displayLoading()
                 searchMovieAPI(query, 1, false)
                 return false
             }
@@ -97,14 +93,7 @@ class MovieListFragment : Fragment(), OnMovieListener {
         })
     }
 
-    override fun onMovieClick(position: Int) {
-        val movieID = adapter!!.getMovieID(position)!!
-        val bundle = Bundle()
-        bundle.putInt("movieID", movieID)
-        navController!!.navigate(R.id.action_movieListFragment_to_movieDetailsFragment, bundle)
-    }
 
-    override fun onCategoryClick(category: String?) {}
     override fun onResume() {
         super.onResume()
         searchMovieAPI("life", Constants.DEFAULT_PAGE, true)
@@ -117,5 +106,9 @@ class MovieListFragment : Fragment(), OnMovieListener {
     override fun onDestroy() {
         super.onDestroy()
         mviewModel.cancelJob("context destroyed!")
+    }
+
+    override fun onItemSelected(position: Int, item: Result) {
+
     }
 }
