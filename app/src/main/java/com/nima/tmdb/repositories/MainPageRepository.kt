6a5d8d1c9ed590aa91b19e974f612284ca0335b8
nima abdpoor <nimaabdi328@yahoo.com>
2020-12-20 14:41:30 +0330

    MainPageRepository.kt created.

--- app/src/main/java/com/nima/tmdb/repositories/MainPageRepository.kt
+++ app/src/main/java/com/nima/tmdb/repositories/MainPageRepository.kt
@@ -1,5 +1,14 @@
 package com.nima.tmdb.repositories
 
-object MainPageRepository {
+import com.nima.tmdb.requests.moshi.Network
+import com.nima.tmdb.utils.Constants.API_KEY
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
+object MainPageRepository {
+    suspend fun getAccountDetails(sessionId: String) {
+        withContext(Dispatchers.IO){
+            Network.service.getAccountDetails(API_KEY , sessionId).await()
+        }
+    }
 }
