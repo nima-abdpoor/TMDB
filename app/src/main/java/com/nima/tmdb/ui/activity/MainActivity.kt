b6506202cda403f7ae937ec0541ebf2bb8178c6b
nima abdpoor <nimaabdi328@yahoo.com>
2020-12-20 15:15:12 +0330

    path sessionId from activity to MainPageFragment.kt

--- app/src/main/java/com/nima/tmdb/ui/activity/MainActivity.kt
+++ app/src/main/java/com/nima/tmdb/ui/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.navigation.findNavController
 import com.nima.tmdb.R
 import com.nima.tmdb.login.Authentication
 import com.nima.tmdb.login.state.LoginStateEvent
@@ -17,13 +18,19 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bundle : Bundle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val sessionId = intent.getStringExtra(R.string.requestToken.toString())
         sessionId?.let {
-            if (sessionId.isEmpty()){
+            if (sessionId.isEmpty()) {
                 //offline Mode
+            }
+            else{
+                bundle = Bundle()
+                bundle.putString(R.string.sessionId.toString() , sessionId)
+                findNavController(R.id.nav_host_fragment).setGraph(R.navigation.nav_graph ,bundle )
             }
         }
         Log.d(TAG, "onCreate: $sessionId")
--- app/src/main/java/com/nima/tmdb/ui/fragments/MainPageFragment.kt
+++ app/src/main/java/com/nima/tmdb/ui/fragments/MainPageFragment.kt
@@ -2,6 +2,7 @@ package com.nima.tmdb.ui.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +12,8 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.nima.tmdb.R
 import com.nima.tmdb.viewModels.MainPageViewModel
-import com.nima.tmdb.viewModels.MovieListViewModel
+
+const val TAG : String = "MainPageFragment"
 
 class MainPageFragment : Fragment() {
     private lateinit var mainPageViewModel: MainPageViewModel
@@ -20,6 +22,11 @@ class MainPageFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainPageViewModel = ViewModelProvider(this).get(MainPageViewModel::class.java)
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+        TODO()
     }
 
     override fun onAttach(context: Context) {
@@ -31,8 +38,9 @@ class MainPageFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_main_page, container, false)
+        val sessionId = savedInstanceState?.getString(R.string.sessionId.toString())
+        Log.d(TAG, "onCreateView: $sessionId")
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
