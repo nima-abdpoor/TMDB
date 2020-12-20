e725923e13745c9a05a57629b188b17cdd4c5075
nima abdpoor <nimaabdi328@yahoo.com>
2020-12-20 14:36:46 +0330

    account api and new retrofit with moshi created.

--- app/src/main/java/com/nima/tmdb/requests/TheMovieDataBaseAPI.kt
+++ app/src/main/java/com/nima/tmdb/requests/TheMovieDataBaseAPI.kt
@@ -37,9 +37,4 @@ interface TheMovieDataBaseAPI {
     @POST("authentication/session/new")
     suspend fun getSessionId(@Body requestToken : RequestToken, @Query("api_key") key: String?) : Session
 
-    @GET("account")
-    suspend fun getAccountDetails(
-        @Query("api_key") key: String,
-        @Query("session_id") sessionId: String,
-    ): Account
 }
--- app/src/main/java/com/nima/tmdb/requests/moshi/MovieApi.kt
+++ app/src/main/java/com/nima/tmdb/requests/moshi/MovieApi.kt
@@ -1,2 +1,14 @@
 package com.nima.tmdb.requests.moshi
 
+import com.nima.tmdb.models.login.account.Account
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApi{
+    @GET("account")
+    suspend fun getAccountDetails(
+        @Query("api_key") key: String,
+        @Query("session_id") sessionId: String,
+    ): Deferred<Account>
+}
