184f05f4fbda473a778a1f95df5a0260a2301cbc
nima abdpoor <nimaabdi328@yahoo.com>
2020-12-20 14:07:11 +0330

    adding account table to database for storing data and creating the room presistence.

--- app/src/main/java/com/nima/tmdb/database/DatabaseEntities.kt
+++ app/src/main/java/com/nima/tmdb/database/DatabaseEntities.kt
@@ -1,2 +1,16 @@
 package com.nima.tmdb.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Account constructor(
+    @PrimaryKey
+    val id: Int,
+    val hash: String,
+    val iso6391: String,
+    val iso31661: String,
+    val name: String,
+    val includeAdult: Boolean,
+    val username: String
+)
