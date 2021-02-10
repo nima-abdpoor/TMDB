7ca901c4c5b2e984b6ad9034f7e39a9e764250a5
nima-abdpoor <nimaabdi328@yahoo.com>
2021-02-10 13:08:43 +0330

    nav graph is ready for LoginFragment.kt

--- app/src/main/java/com/nima/tmdb/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/nima/tmdb/ui/fragments/LoginFragment.kt
@@ -1,5 +1,8 @@
 package com.nima.tmdb.ui.fragments
 
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.nima.tmdb.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -7,4 +10,18 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class LoginFragment :Fragment(R.layout.login_fragment){
 
+    private val TAG = "LoginFragment"
+    private var requestToken: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestToken = arguments?.getString(R.string.requestToken.toString(),"null")
+        Log.d(TAG, "onCreate: $requestToken")
+    }
+
+    object callBack :OnBackPressedCallback(true){
+        override fun handleOnBackPressed() {
+
+        }
+    }
 }
--- app/src/main/java/com/nima/tmdb/ui/fragments/MainPageFragment.kt
+++ app/src/main/java/com/nima/tmdb/ui/fragments/MainPageFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.nima.tmdb.R
 import com.nima.tmdb.database.MyDao
 import com.nima.tmdb.requests.wrapper.ApiWrapper
@@ -78,7 +79,9 @@ class MainPageFragment : Fragment(R.layout.fragment_main_page) {
             ){
                 viewModel.login(userInfo,requestToken, API_KEY)
             }else{
-                // TODO: 2/10/2021 navigate to login Page
+                val bundle = Bundle()
+                bundle.putString(R.string.requestToken.toString(),requestToken)
+                findNavController().navigate(R.id.action_mainPageFragment_to_loginFragment,bundle)
             }
         }
     }
