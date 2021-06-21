e0332d5913ef45aae893ba4aca8f47b4f9fd1b1e
nima-abdpoor <nimaabdi328@yahoo.com>
2021-06-21 22:02:16 +0430

    CreatedLists.kt class for modeling the retrofit response.

---
+++ app/src/main/java/com/nima/tmdb/models/account/lists/CreatedLists.kt
@@ -0,0 +1,23 @@
+package com.nima.tmdb.models.account.lists
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class CreatedLists {
+    @SerializedName("page")
+    @Expose
+    private val page: Int? = null
+
+    @SerializedName("results")
+    @Expose
+    private val results: List<CreatedListsResult>? = null
+
+    @SerializedName("total_pages")
+    @Expose
+    private val totalPages: Int? = null
+
+    @SerializedName("total_results")
+    @Expose
+    private val totalResults: Int? = null
+
+}
---
+++ app/src/main/java/com/nima/tmdb/models/account/lists/CreatedListsResult.kt
@@ -0,0 +1,40 @@
+package com.nima.tmdb.models.account.lists
+
+import com.google.gson.annotations.Expose
+
+import com.google.gson.annotations.SerializedName
+
+
+class CreatedListsResult {
+    @SerializedName("description")
+    @Expose
+    private val description: String? = null
+
+    @SerializedName("favorite_count")
+    @Expose
+    private val favoriteCount: Int? = null
+
+    @SerializedName("id")
+    @Expose
+    private val id: Int? = null
+
+    @SerializedName("item_count")
+    @Expose
+    private val itemCount: Int? = null
+
+    @SerializedName("iso_639_1")
+    @Expose
+    private val iso6391: String? = null
+
+    @SerializedName("list_type")
+    @Expose
+    private val listType: String? = null
+
+    @SerializedName("name")
+    @Expose
+    private val name: String? = null
+
+    @SerializedName("poster_path")
+    @Expose
+    private val posterPath: Any? = null
+}
