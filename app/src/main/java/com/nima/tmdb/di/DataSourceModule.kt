d272f594fa187d0ae691d8088cb34083517ba918
nima <nimaabdpoor@gmail.com>
2021-09-12 17:22:20 +0430

    providing splash, mainPage and login intercators in DataSourceModule.kt

--- app/src/main/java/com/nima/tmdb/di/DataSourceModule.kt
+++ app/src/main/java/com/nima/tmdb/di/DataSourceModule.kt
@@ -4,6 +4,12 @@ import com.nima.tmdb.business.data.cache.abstraction.CacheDataSource
 import com.nima.tmdb.business.data.cache.implementation.CacheDataSourceImpl
 import com.nima.tmdb.business.data.network.abstraction.RemoteDataSource
 import com.nima.tmdb.business.data.network.implementation.RemoteDataSourceImp
+import com.nima.tmdb.business.interactors.login.GetSessionId
+import com.nima.tmdb.business.interactors.login.Login
+import com.nima.tmdb.business.interactors.login.LoginInteractors
+import com.nima.tmdb.business.interactors.mainPage.*
+import com.nima.tmdb.business.interactors.splash.GetToken
+import com.nima.tmdb.business.interactors.splash.SplashInteractors
 import com.nima.tmdb.framewrok.datasource.cache.abstraction.DaoService
 import com.nima.tmdb.framewrok.datasource.cache.implementation.DaoServiceImpl
 import com.nima.tmdb.framewrok.datasource.network.abstraction.RemoteService
@@ -40,5 +46,45 @@ object DataSourceModule {
     @Provides
     fun provideCacheDataSource(cacheService: DaoService): CacheDataSource {
         return CacheDataSourceImpl(cacheService)
+    }
+
+    @Singleton
+    @Provides
+    fun provideLoginInteractors(
+        cacheDataSource: CacheDataSource,
+        remoteDataSource: RemoteDataSource
+    ): LoginInteractors {
+        return LoginInteractors(
+            GetSessionId(cacheDataSource, remoteDataSource),
+            Login(cacheDataSource, remoteDataSource)
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideMainPageInteractors(
+        cacheDataSource: CacheDataSource,
+        remoteDataSource: RemoteDataSource
+    ): MainPageInteractors {
+        return MainPageInteractors(
+            AddToWatchList(cacheDataSource, remoteDataSource),
+            GetAccount(cacheDataSource, remoteDataSource),
+            GetPopularMovie(cacheDataSource, remoteDataSource),
+            GetTrendingMovie(cacheDataSource, remoteDataSource),
+            MarkAsFavorite(cacheDataSource, remoteDataSource)
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideSplashInteractors(
+        cacheDataSource: CacheDataSource,
+        remoteDataSource: RemoteDataSource
+    ): SplashInteractors {
+        return SplashInteractors(
+            GetSessionId(cacheDataSource, remoteDataSource),
+            Login(cacheDataSource, remoteDataSource),
+            GetToken(cacheDataSource, remoteDataSource)
+        )
     }
 }
