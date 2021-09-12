4062a8022ee5c670df5b8119e13b47071320d2db
nima <nimaabdpoor@gmail.com>
2021-09-12 17:00:14 +0430

    login interactors.

---
+++ app/src/main/java/com/nima/tmdb/business/interactors/login/GetSessionId.kt
@@ -0,0 +1,20 @@
+package com.nima.tmdb.business.interactors.login
+
+import com.nima.tmdb.business.data.cache.abstraction.CacheDataSource
+import com.nima.tmdb.business.data.network.abstraction.RemoteDataSource
+import com.nima.tmdb.business.domain.model.login.LoginInfo
+import com.nima.tmdb.business.domain.model.login.LoginResponse
+import com.nima.tmdb.business.domain.model.login.RequestToken
+import com.nima.tmdb.business.domain.model.login.Session
+import com.nima.tmdb.requests.wrapper.ApiWrapper
+
+class GetSessionId(
+    private val cache: CacheDataSource,
+    private val remote: RemoteDataSource
+) {
+
+    suspend fun getSessionId(requestToken: RequestToken, apiKey: String): ApiWrapper<Session> {
+        return remote.getSessionId(requestToken, apiKey)
+    }
+
+}
