5aac25ce98c24d5d3331984f0b45d64b3e91f9ef
nima <nimaabdpoor@gmail.com>
2021-09-12 17:00:32 +0430

    mainPage interactors.

---
+++ app/src/main/java/com/nima/tmdb/business/interactors/mainPage/GetAccount.kt
@@ -0,0 +1,15 @@
+package com.nima.tmdb.business.interactors.mainPage
+
+import com.nima.tmdb.business.data.cache.abstraction.CacheDataSource
+import com.nima.tmdb.business.data.network.abstraction.RemoteDataSource
+import com.nima.tmdb.business.domain.model.login.account.Account
+import com.nima.tmdb.requests.wrapper.ApiWrapper
+
+class GetAccount(
+    private val cache: CacheDataSource,
+    private val remote: RemoteDataSource
+) {
+    suspend fun getAccount(apiKey: String,sessionId : String): ApiWrapper<Account>{
+        return remote.getAccount(apiKey, sessionId)
+    }
+}
