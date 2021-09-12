2b6b1e77276bde1863d560eaaa9677bd1bc388da
nima <nimaabdpoor@gmail.com>
2021-09-12 16:53:31 +0430

    add cacheDataSource classes to business.

---
+++ app/src/main/java/com/nima/tmdb/business/data/cache/implementation/CacheDataSourceImpl.kt
@@ -0,0 +1,8 @@
+package com.nima.tmdb.business.data.cache.implementation
+
+import com.nima.tmdb.business.data.cache.abstraction.CacheDataSource
+import com.nima.tmdb.framewrok.datasource.cache.abstraction.DaoService
+
+class CacheDataSourceImpl(cacheService: DaoService) : CacheDataSource {
+
+}
