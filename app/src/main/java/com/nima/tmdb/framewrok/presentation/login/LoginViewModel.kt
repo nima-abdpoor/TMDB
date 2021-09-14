d13b30bfd189e97a1f0f07fdb8ce4bbb7a928767
nima <nimaabdpoor@gmail.com>
2021-09-14 20:49:43 +0430

    handling LoginViewModel.kt with BaseViewModel.

--- app/src/main/java/com/nima/tmdb/framewrok/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/nima/tmdb/framewrok/presentation/login/LoginViewModel.kt
@@ -1,37 +1,24 @@
 package com.nima.tmdb.framewrok.presentation.login
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.nima.tmdb.business.data.network.requests.wrapper.ApiWrapper
 import com.nima.tmdb.business.domain.model.login.LoginInfo
 import com.nima.tmdb.business.domain.model.login.LoginResponse
 import com.nima.tmdb.business.domain.model.login.RequestToken
-import com.nima.tmdb.business.domain.model.login.Session
 import com.nima.tmdb.business.interactors.login.LoginInteractors
-import com.nima.tmdb.business.data.network.requests.wrapper.ApiWrapper
-import kotlinx.coroutines.launch
+import com.nima.tmdb.framewrok.presentation.common.BaseViewModel
+import kotlinx.coroutines.flow.StateFlow
 
 class LoginViewModel @ViewModelInject constructor(
     private val repository: LoginInteractors
-) : ViewModel() {
+) : BaseViewModel() {
 
-    private val _login = MutableLiveData<ApiWrapper<LoginResponse>>()
-    val login: LiveData<ApiWrapper<LoginResponse>>
-        get() = _login
-
-    private val _sessionId = MutableLiveData<ApiWrapper<Session>>()
-    val sessionId: LiveData<ApiWrapper<Session>>
-        get() = _sessionId
-
-    fun login(loginInfo: LoginInfo, apiKey: String) =
-        viewModelScope.launch {
-            _login.value = repository.login.login(loginInfo, apiKey)
+    fun login(loginInfo: LoginInfo, apiKey: String): StateFlow<ApiWrapper<LoginResponse>> =
+        execute {
+            repository.login.login(loginInfo, apiKey)
         }
 
-    fun getSessionId(requestToken: RequestToken, apiKey: String) =
-        viewModelScope.launch {
-            _sessionId.value = repository.getSessionId.getSessionId(requestToken, apiKey)
-        }
+    fun getSessionId(requestToken: RequestToken, apiKey: String) = execute {
+        repository.getSessionId.getSessionId(requestToken, apiKey)
+    }
 }
