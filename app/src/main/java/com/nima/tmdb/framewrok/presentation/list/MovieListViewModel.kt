c53b7e9dfb89fe5b0ed8ff213d58060d2a870838
nima <nimaabdpoor@gmail.com>
2021-09-14 20:48:36 +0430

    handle MovieListViewModel with BaseViewModel.

--- app/src/main/java/com/nima/tmdb/framewrok/presentation/list/MovieListViewModel.kt
+++ app/src/main/java/com/nima/tmdb/framewrok/presentation/list/MovieListViewModel.kt
@@ -1,21 +1,16 @@
 package com.nima.tmdb.framewrok.presentation.list
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.nima.tmdb.business.data.network.requests.wrapper.ApiWrapper
 import com.nima.tmdb.business.domain.model.Example
 import com.nima.tmdb.business.interactors.list.ListInteractors
-import com.nima.tmdb.utils.Constants.TIME_OUT_MEDIUM
-import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import com.nima.tmdb.framewrok.presentation.common.BaseViewModel
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
 
 
 class MovieListViewModel @ViewModelInject constructor(
     private val repository: ListInteractors
-) : ViewModel() {
+) : BaseViewModel() {
 
     fun setMovie(
         key: String,
@@ -23,16 +18,7 @@ class MovieListViewModel @ViewModelInject constructor(
         query: String,
         page: Int,
         include_adult: Boolean
-    ): StateFlow<ApiWrapper<Example>> {
-        val result: StateFlow<ApiWrapper<Example>> = flow {
-            val re =
-                repository.searchMovie.searchMovieAPI(key, language, query, page, include_adult)
-            emit(re)
-        }.stateIn(
-            scope = viewModelScope,
-            started = WhileSubscribed(TIME_OUT_MEDIUM),
-            initialValue = ApiWrapper.Loading("loading")
-        )
-        return result
+    ): StateFlow<ApiWrapper<Example>> = execute {
+        repository.searchMovie.searchMovieAPI(key, language, query, page, include_adult)
     }
 }
