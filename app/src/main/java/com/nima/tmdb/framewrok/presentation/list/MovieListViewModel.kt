3c555e4406acc7103dd18a8926e328248f12e00f
nima <nimaabdpoor@gmail.com>
2021-09-14 17:22:19 +0430

    emit values from repository to Flow builder with StateIn method.

--- app/src/main/java/com/nima/tmdb/framewrok/presentation/list/MovieListViewModel.kt
+++ app/src/main/java/com/nima/tmdb/framewrok/presentation/list/MovieListViewModel.kt
@@ -1,28 +1,38 @@
 package com.nima.tmdb.framewrok.presentation.list
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.nima.tmdb.business.data.network.requests.wrapper.ApiWrapper
 import com.nima.tmdb.business.domain.model.Example
 import com.nima.tmdb.business.interactors.list.ListInteractors
-import com.nima.tmdb.business.data.network.requests.wrapper.ApiWrapper
-import kotlinx.coroutines.launch
+import com.nima.tmdb.utils.Constants.TIME_OUT_MEDIUM
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 
 
 class MovieListViewModel @ViewModelInject constructor(
     private val repository: ListInteractors
 ) : ViewModel() {
 
-    private val _movieList = MutableLiveData<ApiWrapper<Example>>()
-    val movieList: LiveData<ApiWrapper<Example>>
-        get() = _movieList
-
-
-    fun setMovie(key: String, language: String?, query: String, page: Int, include_adult: Boolean) =
-        viewModelScope.launch {
-            _movieList.value =
+    fun setMovie(
+        key: String,
+        language: String?,
+        query: String,
+        page: Int,
+        include_adult: Boolean
+    ): StateFlow<ApiWrapper<Example>> {
+        val result: StateFlow<ApiWrapper<Example>> = flow {
+            val re =
                 repository.searchMovie.searchMovieAPI(key, language, query, page, include_adult)
-        }
+            emit(re)
+        }.stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(TIME_OUT_MEDIUM),
+            initialValue = ApiWrapper.Loading("loading")
+        )
+        return result
+    }
 }
