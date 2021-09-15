79f74f2cadf07c0f2130420659f5aaeb1b02a963
nima <nimaabdpoor@gmail.com>
2021-09-15 22:15:57 +0430

    handle CreatedListViewModel.kt with BaseViewModel.

--- app/src/main/java/com/nima/tmdb/framewrok/presentation/createdlist/CreatedListViewModel.kt
+++ app/src/main/java/com/nima/tmdb/framewrok/presentation/createdlist/CreatedListViewModel.kt
@@ -1,21 +1,12 @@
 package com.nima.tmdb.framewrok.presentation.createdlist
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.nima.tmdb.business.domain.model.account.lists.CreatedLists
 import com.nima.tmdb.business.interactors.createdList.CreatedListInteractors
-import com.nima.tmdb.business.data.network.requests.wrapper.ApiWrapper
-import kotlinx.coroutines.launch
+import com.nima.tmdb.framewrok.presentation.common.BaseViewModel
 
 class CreatedListViewModel @ViewModelInject constructor(
     private val repository: CreatedListInteractors
-) : ViewModel() {
-    private val _createdLists = MutableLiveData<ApiWrapper<CreatedLists>>()
-    val createdLists: LiveData<ApiWrapper<CreatedLists>>
-        get() = _createdLists
+) : BaseViewModel() {
 
     fun getCreatedLists(
         accountId: String,
@@ -23,15 +14,13 @@ class CreatedListViewModel @ViewModelInject constructor(
         sessionId: String,
         language: String?,
         page: Int?
-    ) =
-        viewModelScope.launch {
-            _createdLists.value =
-                repository.getCreatedList.getCreatedLists(
-                    accountId,
-                    sessionId,
-                    apiKey,
-                    language,
-                    page
-                )
-        }
+    ) = execute {
+        repository.getCreatedList.getCreatedLists(
+            accountId,
+            sessionId,
+            apiKey,
+            language,
+            page
+        )
+    }
 }
