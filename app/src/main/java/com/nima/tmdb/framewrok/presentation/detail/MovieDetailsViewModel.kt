6c063a5b539d521d5737648a064bd8fe2a791dcd
nima <nimaabdpoor@gmail.com>
2021-09-15 22:18:01 +0430

    handle MovieDetailsViewModel.kt.kt with BaseViewModel.

--- app/src/main/java/com/nima/tmdb/framewrok/presentation/detail/MovieDetailsViewModel.kt
+++ app/src/main/java/com/nima/tmdb/framewrok/presentation/detail/MovieDetailsViewModel.kt
@@ -1,26 +1,14 @@
 package com.nima.tmdb.framewrok.presentation.detail
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.nima.tmdb.business.domain.model.Details
 import com.nima.tmdb.business.interactors.details.DetailsInteractors
-import com.nima.tmdb.business.data.network.requests.wrapper.ApiWrapper
-import kotlinx.coroutines.launch
+import com.nima.tmdb.framewrok.presentation.common.BaseViewModel
 
 class MovieDetailsViewModel @ViewModelInject constructor(
     private val repository: DetailsInteractors
-) : ViewModel() {
+) : BaseViewModel() {
 
-    private val _movieDetails = MutableLiveData<ApiWrapper<Details>>()
-    val movieDetails: LiveData<ApiWrapper<Details>>
-        get() = _movieDetails
-
-
-    fun setMovieID(movieId: Int, apiKey: String, language: String) =
-        viewModelScope.launch {
-            _movieDetails.value = repository.getMovieById.searchMovieAPI(movieId, apiKey, language)
-        }
+    fun setMovieID(movieId: Int, apiKey: String, language: String) = execute {
+        repository.getMovieById.searchMovieAPI(movieId, apiKey, language)
+    }
 }
